3853b448cd9e3d98e8ec95415ff6952b43a12d93
Santiago Mattiauda <santimattius@gmail.com>
2023-11-08 22:12:00 -0300

    [feature] update implementation

--- shared/src/commonMain/kotlin/com.santimattius.kmp.entertainment/core/extensions/ViewModels.kt
+++ shared/src/commonMain/kotlin/com.santimattius.kmp.entertainment/core/extensions/ViewModels.kt
@@ -3,7 +3,7 @@ package com.santimattius.kmp.entertainment.core.extensions
 import androidx.compose.runtime.Composable
 import moe.tlaster.precompose.viewmodel.ViewModel
 import moe.tlaster.precompose.viewmodel.viewModel
-import org.koin.compose.koinInject
+import org.koin.compose.LocalKoinScope
 import org.koin.core.parameter.ParametersDefinition
 
 /**
@@ -15,6 +15,6 @@ import org.koin.core.parameter.ParametersDefinition
  */
 @Composable
 inline fun <reified T : ViewModel> koinViewModel(noinline parameters: ParametersDefinition? = null): T {
-    val newViewModelInstance = koinInject<T>(parameters = parameters)
-    return viewModel { newViewModelInstance }
+    val scope = LocalKoinScope.current
+    return viewModel { scope.get(parameters = parameters) }
 }
