2436ed88053bcbecf193e0d02385849292def25c
Santiago Mattiauda <santimattius@gmail.com>
2024-07-04 19:47:03 -0300

    [update] update kotlin, compose compiler, room (#12)

--- shared/src/commonMain/kotlin/com.santimattius.kmp.entertainment/core/db/TMDBDataBase.kt
+++ shared/src/commonMain/kotlin/com.santimattius.kmp.entertainment/core/db/TMDBDataBase.kt
@@ -3,8 +3,16 @@ package com.santimattius.kmp.entertainment.core.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
+interface DB {
+    fun clearAllTables() {}
+}
+
 @Database(entities = [FavoriteEntity::class], version = 1)
-abstract class TMDBDataBase : RoomDatabase() {
+abstract class TMDBDataBase : RoomDatabase(), DB {
 
     abstract fun getFavoriteDao(): FavoriteDao
+
+    override fun clearAllTables() {
+        super.clearAllTables()
+    }
 }
