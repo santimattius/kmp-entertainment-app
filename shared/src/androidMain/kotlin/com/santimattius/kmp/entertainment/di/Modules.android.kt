4d2d57ca3e1134cb8a1dcd4e33adea511aa08c2b
Santiago Mattiauda <santimattius@gmail.com>
2025-01-25 13:45:05 -0300

    [feature] update koin version and remove ContextInitializer.kt (#18)
    * [feature] update koin version and remove ContextInitializer.kt
    
    * Update libs.versions.toml

--- shared/src/androidMain/kotlin/com/santimattius/kmp/entertainment/di/Modules.android.kt
+++ shared/src/androidMain/kotlin/com/santimattius/kmp/entertainment/di/Modules.android.kt
@@ -1,20 +1,19 @@
 package com.santimattius.kmp.entertainment.di
 
-import android.content.Context
-import com.santimattius.kmp.entertainment.applicationContext
 import com.santimattius.kmp.entertainment.core.db.RoomFactory
 import com.santimattius.kmp.entertainment.core.db.TMDBDataBase
 import com.santimattius.kmp.entertainment.core.db.getRoomDatabase
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 actual val platformModule: Module
     get() = module {
-        factory<Context> {
-            val context = applicationContext ?: run {
-                throw IllegalArgumentException("Application context is null")
-            }
-            context
+        single<TMDBDataBase> {
+            getRoomDatabase(
+                builder = RoomFactory(
+                    context = androidContext()
+                ).create()
+            )
         }
-        single<TMDBDataBase> { getRoomDatabase(RoomFactory(get()).create()) }
     }
