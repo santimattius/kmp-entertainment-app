c256f7061d3b1dd6d7a4ef5ad818279b8fe2e79b
Santiago Mattiauda <santimattius@gmail.com>
2025-03-22 16:55:58 -0300

    [experimental] compose preview

--- shared/src/commonMain/kotlin/com.santimattius.kmp.entertainment/feature/splash/Splash.kt
+++ shared/src/commonMain/kotlin/com.santimattius.kmp.entertainment/feature/splash/Splash.kt
@@ -1,10 +1,13 @@
 package com.santimattius.kmp.entertainment.feature.splash
 
 import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.AnimationVector1D
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
@@ -13,8 +16,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.layout.ContentScale
 import com.santimattius.kmp.entertainment.core.ui.components.NetworkImage
+import com.santimattius.kmp.entertainment.core.ui.themes.AppTheme
+import com.santimattius.kmp.entertainment.shared.generated.resources.Res
+import com.santimattius.kmp.entertainment.shared.generated.resources.compose_multiplatform
 import kotlinx.coroutines.delay
-
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.jetbrains.compose.ui.tooling.preview.PreviewParameter
 
 @Composable
 fun SplashScreen(navigate: () -> Unit) {
@@ -34,21 +42,34 @@ fun SplashScreen(navigate: () -> Unit) {
         navigate()
     }
 
+    SplashComponent(modifier=Modifier.scale(scale.value))
+}
+
+@Composable
+private fun SplashComponent(modifier: Modifier = Modifier) {
     Box(
         contentAlignment = Alignment.Center,
         modifier = Modifier.fillMaxSize()
     ) {
-//        Image(
-//            painter = painterResource(Res.drawable.compose_multiplatform),
-//            modifier = Modifier.scale(scale.value),
-//            contentDescription = null
-//        )
-        NetworkImage(
+        Image(
+            painter = painterResource(Res.drawable.compose_multiplatform),
+            modifier = modifier,
+            contentDescription = null
+        )
+        /*NetworkImage(
             imageUrl = "https://img.freepik.com/vector-gratis/produccion-peliculas-cine-composicion-transparente-realista-imagen-aislada-badajo-campos-vacios-ilustracion-vectorial_1284-66163.jpg?w=740&t=st=1694904624~exp=1694905224~hmac=936e49d4984b5c24111a43ff7835c3962d6cdf002d489f5ed196716577f7d2c3",
             contentDescription = "Logo",
-            modifier = Modifier.scale(scale.value),
+            modifier = modifier,
             contentScale = ContentScale.Crop
-        )
+        )*/
 
+    }
+}
+
+@Preview
+@Composable
+fun SplashPreview() {
+    AppTheme {
+        SplashComponent()
     }
 }
