035683e44dd680e60f86dda34e7c5a0fcb41436d
Santiago Mattiauda <santimattius@gmail.com>
2025-04-01 19:51:19 -0300

    [update] update image and example

--- shared/src/commonMain/kotlin/com.santimattius.kmp.entertainment/core/ui/components/NetworkImage.kt
+++ shared/src/commonMain/kotlin/com.santimattius.kmp.entertainment/core/ui/components/NetworkImage.kt
@@ -2,27 +2,27 @@ package com.santimattius.kmp.entertainment.core.ui.components
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.unit.dp
 import coil3.compose.LocalPlatformContext
 import coil3.compose.SubcomposeAsyncImage
 import coil3.request.ImageRequest
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.unit.dp
 
 @Composable
 internal fun NetworkImage(
-    imageUrl: String,
+    image: Any,
     modifier: Modifier = Modifier,
     contentScale: ContentScale,
     contentDescription: String? = null,
 ) {
     SubcomposeAsyncImage(
         model = ImageRequest.Builder(LocalPlatformContext.current)
-            .data(imageUrl).build(),
+            .data(image).build(),
         loading = {
             Box(contentAlignment = Alignment.Center) {
                 CircularProgressIndicator(
--- shared/src/commonMain/kotlin/com.santimattius.kmp.entertainment/feature/splash/Splash.kt
+++ shared/src/commonMain/kotlin/com.santimattius.kmp.entertainment/feature/splash/Splash.kt
@@ -1,28 +1,33 @@
 package com.santimattius.kmp.entertainment.feature.splash
 
 import androidx.compose.animation.core.Animatable
-import androidx.compose.animation.core.AnimationVector1D
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
-import androidx.compose.ui.layout.ContentScale
-import com.santimattius.kmp.entertainment.core.ui.components.NetworkImage
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import coil3.ColorImage
+import coil3.annotation.ExperimentalCoilApi
+import coil3.compose.AsyncImagePreviewHandler
+import coil3.compose.LocalAsyncImagePreviewHandler
 import com.santimattius.kmp.entertainment.core.ui.themes.AppTheme
 import com.santimattius.kmp.entertainment.shared.generated.resources.Res
-import com.santimattius.kmp.entertainment.shared.generated.resources.compose_multiplatform
+import com.santimattius.kmp.entertainment.shared.generated.resources.watching_a_movie
 import kotlinx.coroutines.delay
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import org.jetbrains.compose.ui.tooling.preview.PreviewParameter
+
+private const val SPLASH_IMAGE =
+    "https://img.freepik.com/vector-gratis/produccion-peliculas-cine-composicion-transparente-realista-imagen-aislada-badajo-campos-vacios-ilustracion-vectorial_1284-66163.jpg?w=740&t=st=1694904624~exp=1694905224~hmac=936e49d4984b5c24111a43ff7835c3962d6cdf002d489f5ed196716577f7d2c3"
 
 @Composable
 fun SplashScreen(navigate: () -> Unit) {
@@ -42,7 +47,7 @@ fun SplashScreen(navigate: () -> Unit) {
         navigate()
     }
 
-    SplashComponent(modifier=Modifier.scale(scale.value))
+    SplashComponent(modifier = Modifier.scale(scale.value))
 }
 
 @Composable
@@ -52,24 +57,31 @@ private fun SplashComponent(modifier: Modifier = Modifier) {
         modifier = Modifier.fillMaxSize()
     ) {
         Image(
-            painter = painterResource(Res.drawable.compose_multiplatform),
+            painter = painterResource(Res.drawable.watching_a_movie),
             modifier = modifier,
             contentDescription = null
         )
+        //TODO: test coil preview
         /*NetworkImage(
-            imageUrl = "https://img.freepik.com/vector-gratis/produccion-peliculas-cine-composicion-transparente-realista-imagen-aislada-badajo-campos-vacios-ilustracion-vectorial_1284-66163.jpg?w=740&t=st=1694904624~exp=1694905224~hmac=936e49d4984b5c24111a43ff7835c3962d6cdf002d489f5ed196716577f7d2c3",
+            image = SPLASH_IMAGE,
             contentDescription = "Logo",
             modifier = modifier,
             contentScale = ContentScale.Crop
         )*/
-
     }
 }
 
+@OptIn(ExperimentalCoilApi::class)
 @Preview
 @Composable
 fun SplashPreview() {
     AppTheme {
-        SplashComponent()
+        //TODO: test coil preview
+        val previewHandler = AsyncImagePreviewHandler {
+            ColorImage(Color.Red.toArgb())
+        }
+        CompositionLocalProvider(LocalAsyncImagePreviewHandler provides previewHandler) {
+            SplashComponent()
+        }
     }
 }
